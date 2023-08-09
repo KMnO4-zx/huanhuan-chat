f1c883c660284ea476f841397eee8cdc8e1bab09
dengyuwen <2112164129@e.gzhu.edu.cn>
2023-08-09 23:49:58 +0800

    增加log模块

---
+++ data/log_demo_single.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(CUR_DIR, ".."))
+
+from log.logutli import Logger
+
+if __name__ == "__main__":
+
+    log_id     = 'log_demo_single'  
+    log_dir    = f'./log/result/'
+    log_name   = 'test_log_single.log'
+    log_level  = 'info'
+
+    # 初始化日志
+    logger = Logger(log_id, log_dir, log_name, log_level).logger
+    logger.info('test info')
+    logger.error('test error')
+    logger.warning('test warning')
+    logger.debug('test debug')
+    logger.info('测试单独定义logger')
+    
